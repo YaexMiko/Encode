a614b8a4b1dd9f02853f01c059d76a28a0c055d6
YaexMiko <aloksinghmains756@gmail.com>
2025-02-04 00:22:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config(object):
     ADMIN = int(os.environ.get("ADMIN", "8108281129")) # ⚠️ Required
     LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002381050327')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
-    START_PIC = os.environ.get("START_PIC", "https://graph.org/file/70fc16fcba8f92685433f-f9125e16c551344ad3.jpg")
+    START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/af22bcdafa496d1f0c04c-d2bb1b5fbe230b18ac.jpg")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
@@ -28,13 +28,15 @@ class Config(object):
 
 
     caption = """
-**File Name**: {0}
+────────────────────────────
+**ғɪʟᴇ ɴᴀᴍᴇ**: {0}
 
-**Original File Size:** {1}
-**Encoded File Size:** {2}
-**Compression Percentage:** {3}
-
-__Downloaded in {4}__
-__Encoded in {5}__
-__Uploaded in {6}__
+**ᴏʀɪɢɪɴᴀʟ ғɪʟᴇ sɪᴢᴇ:** {1}
+**ᴇɴᴄᴏᴅᴇᴅ ғɪʟᴇ sɪᴢᴇ:** {2}
+**ᴄᴏᴍᴘʀᴇssɪᴏɴ ᴘᴇʀᴄᴇɴᴛᴀɢᴇ:** {3}
+────────────────────────────
+__ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅ ɪɴ{4}__
+__ ᴠɪᴅᴇᴏ ᴇɴᴄᴏᴅᴇᴅ ɪɴ{5}__
+__ᴠɪᴅᴇᴏ ᴜᴘʟᴏᴀᴅᴇ ɪɴ{6}__
+────────────────────────────
 """
